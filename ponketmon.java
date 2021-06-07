7bdd4c1004e37fa48c84cbe5a4a5739f28727103
shin <zerll9100@gmail.com>
2021-06-07 23:25:15 +0900

    포켓몬 수정중

--- ponketmon.java
+++ ponketmon.java
@@ -1,6 +1,14 @@
 class Solution{
 
     public static void main(String[] args) {
-        System.out.println("hey");
+        int[] nums = {3,3,3,2,2,4};
+        int ponketmonPick = (nums.length / 2);
+        System.out.println(ponketmonPick);
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < ponketmonPick; j++){
+                System.out.println(nums[i]);
+            }
+        }
     }
 }
