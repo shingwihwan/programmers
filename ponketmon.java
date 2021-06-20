e22eae7772f42de16b57bc85bb0edaad9fb2c55c
shin <zerll9100@gmail.com>
2021-06-20 23:43:45 +0900

    수정하는중

--- ponketmon.java
+++ ponketmon.java
@@ -3,12 +3,13 @@ class Solution{
     public static void main(String[] args) {
         int[] nums = {3,3,3,2,2,4};
         int ponketmonPick = (nums.length / 2);
-        System.out.println(ponketmonPick);
+        // System.out.println("pick : " + ponketmonPick);
 
         for(int i = 0; i < nums.length; i++){
-            for(int j = 0; j < ponketmonPick; j++){
-                System.out.println(nums[i]);
-            }
+           for(int j = 0; j < ponketmonPick; j++){
+               System.out.println(nums[i]);
+           }
+            
         }
     }
 }
