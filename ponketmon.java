91b411dc3dcccdfbf7cec2f4dbd9bda4b34afc28
shin <zerll9100@gmail.com>
2021-08-20 01:09:47 +0900

    폰켓몬 다른 사람 풀이 추가

--- ponketmon.java
+++ ponketmon.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class ponketmon {
     public static void main(String[] args) {
@@ -19,9 +21,11 @@ class PonketmonSolve {
 
         for(int data : nums){
             if(!save.contains(data)){
+                System.out.println("nums : " + data);
                 save.add(data);
             }
         }
+
         int cnt = save.size();
         if(ponketmonPick < cnt){
             answer = ponketmonPick;
@@ -29,15 +33,32 @@ class PonketmonSolve {
             answer = cnt;
         }
 
-        System.out.println(answer);
+        return answer;
+    }
+}
 
-        // for(int i = 0; i < nums.length; i++){
-        //    for(int j = 0; j < ponketmonPick; j++){
-        //        System.out.println(nums[i]);
-        //    }
-            
-        // }
-        // System.out.println(nums);
-        return 0;
+// 다른 사람의 풀이
+class Solution1 {
+    public int solution(int[] nums) {
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
+    }
+}
+
+class Solution2 {
+    public int solution(int[] nums) {
+
+            HashSet<Integer> hs = new HashSet<>();
+
+            for(int i =0; i<nums.length;i++) {
+                hs.add(nums[i]);
+            }
+
+            if(hs.size()>nums.length/2)
+                return nums.length/2;
+
+            return hs.size();
     }
 }
