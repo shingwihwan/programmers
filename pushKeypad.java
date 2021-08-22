be1235432b99f1f6a6325a70d9cb60e61472361a
shin <zerll9100@gmail.com>
2021-08-22 23:47:42 +0900

    [카카오인턴]키패드 누르기 시작

---
+++ pushKeypad.java
@@ -0,0 +1,20 @@
+public class pushKeypad {
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
+        String hand = "right";
+        
+        PushKeypadSolve solve = new PushKeypadSolve();
+        solve.solutionPush(numbers, hand);
+    
+    }
+}
+
+class PushKeypadSolve {
+
+    public String solutionPush(int[] numbers, String hand){
+
+        String answer = "";
+
+        return answer;
+    }
+}
