85f492a431aa4a97f1de77ed14b3988931a5d448
shin <zerll9100@gmail.com>
2021-08-25 23:05:49 +0900

    크레인 인형뽑기 게임

---
+++ DoleDrawing.java
@@ -0,0 +1,19 @@
+public class DoleDrawing{
+    public static void main(String[] args) {
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4}
+
+        SolutionDoleDrawing solv = new SolutionDoleDrawing();
+        solv.solution(board, moves);
+        
+    }
+}
+
+class SolutionDoleDrawing {
+    public String solution (int[][] board, int[] moves){
+        String answer = "";
+
+        return answer;
+    }
+
+}
